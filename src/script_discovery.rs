749cdf8f80832f9885dd69c36e67211089510ef5
RoyPrinsGH <royprinsgm@gmail.com>
2026-03-23 20:57:49 +0100

    Hand refactor script discovery a bit

--- src/script_discovery.rs
+++ src/script_discovery.rs
@@ -3,6 +3,9 @@ use std::ffi::OsStr;
 use std::path::Path;
 use std::path::PathBuf;
 
+use crate::errors::JaoError;
+use crate::errors::JaoResult;
+
 pub fn enumerate_scripts_in(root: impl AsRef<Path>) -> impl Iterator<Item = PathBuf> {
     WalkDir::new(root)
         .into_iter()
@@ -10,32 +13,26 @@ pub fn enumerate_scripts_in(root: impl AsRef<Path>) -> impl Iterator<Item = Path
         .filter(|entry| entry.file_type().is_file())
         .filter_map(|entry| {
             let ext = Path::new(entry.file_name()).extension()?.to_str()?;
-            if is_supported_script_extension(ext) {
-                Some(entry.path())
-            } else {
-                None
-            }
+            if is_supported_script_extension(ext) { Some(entry.path()) } else { None }
         })
 }
 
 fn is_supported_script_extension(ext: &str) -> bool {
-    if cfg!(windows) {
-        ext.eq_ignore_ascii_case("bat")
-    } else {
-        ext.eq_ignore_ascii_case("sh")
-    }
+    #[cfg(windows)]
+    return ext.eq_ignore_ascii_case("bat");
+
+    #[cfg(unix)]
+    return ext.eq_ignore_ascii_case("sh");
 }
 
-pub fn resolve_script(root: impl AsRef<Path>, parts: &[String]) -> Option<PathBuf> {
+pub fn resolve_script(root: impl AsRef<Path>, parts: &[String]) -> JaoResult<PathBuf> {
     let script_name = parts.join(".");
-
-    enumerate_scripts_in(root).find(|path| {
-        path.file_stem()
-            .is_some_and(|file_stem| is_script_match(file_stem, &script_name))
-    })
+    enumerate_scripts_in(root)
+        .find(|path| path.file_stem().is_some_and(|file_stem| is_script_name_match(file_stem, &script_name)))
+        .ok_or(JaoError::ScriptNotFound { script_name })
 }
 
-fn is_script_match(file_stem: &OsStr, script_name: &str) -> bool {
+fn is_script_name_match(file_stem: &OsStr, script_name: &str) -> bool {
     let Some(file_stem) = file_stem.to_str() else {
         return false;
     };
